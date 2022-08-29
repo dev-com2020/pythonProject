54492ba0abbeec515deb3e0f5875ad7cda63fb3f
devcom2020 <kwp@tlen.pl>
2022-08-29 13:03:24 +0200

    poniedziałek 13:03

--- bookr/reviews/tests.py
+++ bookr/reviews/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+from bookr.bookr import settings
+
+if settings.DEBUG:
+    pass
+    # wykonaj jakiś kod
--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+# def index(request):
+#     name = request.GET.get("name") or "nieznajomy"
+#     return HttpResponse("Hej, {}! Czy ja się tutaj wyświetlam?".format(name))
+
 def index(request):
-    name = request.GET.get("name") or "nieznajomy"
-    return HttpResponse("Hej, {}! Czy ja się tutaj wyświetlam?".format(name))
+    name = "Świecie"
+    return render(request, "base.html", {"name": name})
