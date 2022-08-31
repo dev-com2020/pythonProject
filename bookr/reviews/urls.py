d50d22fe0ee57f852ce174c265770385f56c906b
devcom2020 <kwp@tlen.pl>
2022-08-31 15:34:21 +0200

    sroda 15:34

--- bookr/reviews/urls.py
+++ bookr/reviews/urls.py
@@ -6,6 +6,8 @@ from reviews import views
 
 from reviews.views import IndexView
 
+from reviews import api_views
+
 urlpatterns = [path('', views.index, name="index"),
                path('books/', views.book_list, name="book_list"),
                path('book/<int:pk>/', views.book_detail, name="book_detail"),
@@ -17,5 +19,9 @@ urlpatterns = [path('', views.index, name="index"),
                path('publishers/new/', views.publisher_edit, name="publisher_create"),
                path('books/profile', TemplateView.as_view(template_name='profile.html')),
                path('test', IndexView.as_view(), name='index_view'),
+               path('api/first/', api_views.first_api_view),
+               path('api/all/', api_views.all_books, name='all_books'),
+               path('api/login', api_views.Login.as_view(), name='login'),
+
 
                ]
